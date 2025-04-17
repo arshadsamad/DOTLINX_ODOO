1bc03b31ac78255300c367c2eddf5500556f72f0
arshad <arshadsamad27@gmail.com>
2025-04-17 16:33:53 +0400

    code to remove see all in landingand seller home page

--- invoice_report/models/account_move.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models, api, _
-
-class AccountMove(models.Model):
-	_inherit = 'account.move'
-
-	extra_notes = fields.Text('Notes')
--- purchase_report/models/purchase_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api, _
-
-class PurchaseOrder(models.Model):
-	_inherit = 'purchase.order'
-
-	extra_notes = fields.Text('Extra Notes')
